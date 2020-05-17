8a2f422dab81df131832c768c5f4ab653d63d163
shahkartik03 <shahkartik.03@gmail.com>
2020-05-17 18:17:01 +0530

    Initial Commit for the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Board from "./Board";
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App_header">TASK MANAGEMENT</header>
+        <Board />
+    </div>
+  );
+}
+
+export default App;
